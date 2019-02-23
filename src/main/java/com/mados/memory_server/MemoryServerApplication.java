ed0eff9ecd7bc60fbee75c4f271d7418be3a6019
bharathambati <bharath.ambati@seamless.se>
2019-02-23 14:33:55 +0530

    template

---
+++ src/main/java/com/mados/memory_server/MemoryServerApplication.java
@@ -0,0 +1,13 @@
+package com.mados.memory_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryServerApplication.class, args);
+	}
+
+}
